9164c231bb794726325aab764e9ffa39a6a54ba7
Aniketh Singh <aniketh9410280466@gmail.com>
2025-04-20 21:29:06 +0530

    loading page done

---
+++ script.js
@@ -0,0 +1,45 @@
+function loadingAnimation() {
+  //loader remove animation
+  var tl = gsap.timeline();
+  tl.from(".line h1", {
+    y: 150,
+    stagger: 0.25,
+    duration: 0.6,
+    delay: 0.5,
+  });
+  tl.from("#line1-part1 , .line h2", {
+    opacity: 0,
+    onStart: function () {
+      var time = document.querySelector("#line1-part1 h5");
+      var count = 0;
+      var Interval = setInterval(function () {
+        count++;
+        if (count <= 100) {
+          time.innerHTML = count;
+        } else {
+          clearInterval(Interval);
+        }
+      }, 33);
+    },
+  });
+  tl.to(".line h2", {
+    animationName: "anime",
+    opacity: 1,
+  });
+  tl.to("#loader", {
+    opacity: 0,
+    duration: 0.4,
+    delay: 4,
+  });
+  tl.from("#page1", {
+    delay: 0.2,
+    y: 1200,
+    opacity: 0,
+    duration: 0.5,
+    ease: Power4,
+  });
+  tl.to("#loader", {
+    display: none,
+  });
+}
+loadingAnimation();
